706b30b3158763c66a4556a544fe3a9801d61000
Ben Grewell <bgrewell@gmail.com>
2019-12-23 16:55:11 -0800

    login and api requests working

--- htbclient.py
+++ htbclient.py
@@ -2,31 +2,45 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+
 class HTBClient(object):
 
-    login_url = "https://www.hackthebox.eu/login"
+    login_url = 'https://www.hackthebox.eu/login'
     session = None
-    headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36", 'Referer': "https://www.hackthebox.eu/"}
-    csrf_token = None
+    additional_headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36', 'Referer': 'https://www.hackthebox.eu/'}
+    _token = None
+    logged_in = False
+    verify_cert = True
 
-    def __init__(self):
+    def __init__(self, verify_cert=True):
+        self.verify_cert = verify_cert
         pass
 
-    def Login(self, username, password):
-        data = {}
+    def login(self, username, password):
         self.session = requests.Session()
-        self.session.headers.update(self.headers)
-        response = self.session.get(self.login_url)
-        soup = BeautifulSoup(response.text)
-        self.csrf_token = soup.find('input', {'name': '_token'})['value']
-        data['_token'] = self.csrf_token
-        data['email'] = username
-        data['password'] = password
-        response = self.session.post(self.login_url, headers=self.headers, data=data)
-        print(response)
+        self.session.headers.update(self.additional_headers)
+        response = self.session.get(self.login_url, verify=self.verify_cert)
+        soup = BeautifulSoup(response.text, features='lxml')
+        self._token = soup.find('input', {'name': '_token'})['value']
+        data = {'_token': self._token, 'email': username, 'password': password}
+        response = self.session.post(self.login_url, data=data, verify=self.verify_cert)
+        soup = BeautifulSoup(response.text, features='lxml')
+        csrf_token = soup.select_one('meta[name="csrf-token"]')['content']
+        response = self.session.get("https://www.hackthebox.eu/home/machines", verify=self.verify_cert)
+        auth_key_lindex = response.text.find("apiToken\":\"") + 11              # TODO: Clean up, sloppy way to get the apiToken
+        auth_key_rindex = response.text.find("\"", auth_key_lindex)
+        auth_key = response.text[auth_key_lindex:auth_key_rindex]
+        self.session.headers.update({'X-CSRF-TOKEN': csrf_token, 'X-Requested-With': 'XMLHttpRequest',
+                                     'X-XSRF-TOKEN': self._token, 'Authorization': 'Bearer ' + auth_key})
+        self.logged_in = response.status_code == 200
+        return self.logged_in
 
-
-
+    def list_machines(self):
+        list_machines_url = 'https://www.hackthebox.eu/api/machines/get/all'
+        if not self.logged_in:
+            raise ConnectionError('You are not logged in. You must first call login()')
+        response = self.session.get(list_machines_url, verify=self.verify_cert)
+        print(response.text)
 
 
 if __name__ == '__main__':
@@ -34,5 +48,7 @@ if __name__ == '__main__':
     htb_user = os.getenv('HTB_USER')
     htb_pass = os.getenv('HTB_PASS')
 
-    client = HTBClient()
-    client.Login(htb_user, htb_pass)
+    client = HTBClient(verify_cert=False)
+    logged_in = client.login(htb_user, htb_pass)
+    print("Logged in: " + str(logged_in))
+    client.list_machines()
