57709d6dfda31665f798120bc128cd344dbcb812
Ben Grewell <bgrewell@gmail.com>
2019-12-23 20:20:19 -0800

    first working version

--- htbclient.py
+++ htbclient.py
@@ -1,14 +1,16 @@
 import os
 import json
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
 class MachineDetails:
 
-    def __init__(self, id, name, os, ip, avatar, avatar_thumb, points, release, retired_date, maker, maker2, ratings_pro, ratings_sucks, user_blood, root_blood, user_owns, root_owns):
-        self.id = id
+    def __init__(self, identifier, name, operating_system, ip, avatar, avatar_thumb, points, release, retired_date, maker, maker2, ratings_pro, ratings_sucks, user_blood, root_blood, user_owns, root_owns):
+        self.identifier = identifier
         self.name = name
-        self.os = os
+        self.operating_system = operating_system
         self.ip = ip
         self.avatar = avatar
         self.avatar_thumb = avatar_thumb
@@ -53,14 +55,15 @@ class MachineDetails:
     def __repr__(self):
         return self.name
 
+
 class Machine:
     """
     Class to represent the HTB machines
     """
-    def __init__(self, id, name, os, ip, avatar_thumb, points, release, retired_date, maker, maker2, rating, user_owns, root_owns, retired, free):
-        self.id = id
+    def __init__(self, identifier, name, operating_system, ip, avatar_thumb, points, release, retired_date, maker, maker2, rating, user_owns, root_owns, retired, free):
+        self.identifier = identifier
         self.name = name
-        self.os = os
+        self.operating_system = operating_system
         self.ip = ip
         self.avatar_thumb = avatar_thumb
         self.points = points
@@ -79,15 +82,6 @@ class Machine:
 
     def __repr__(self):
         return self.name
-
-    def obj_to_dict(self):
-        obj_dict = {
-            "__class__": self.__class__.__name__,
-            "__module__": self.__module__
-        }
-
-        obj_dict.update(self.__dict__)
-        return obj_dict
 
     @staticmethod
     def json_to_machine(json_dict):
@@ -154,25 +148,121 @@ class HTBClient(object):
             machines.append(machine)
         return machines
 
-    def get_machine(self, id):
+    def machine(self, identifier):
         if not self.logged_in:
             raise ConnectionError('You are not logged in. You must first call login()')
-        url = "https://www.hackthebox.eu/api/machines/get/{id}".format(id=id)
+        url = "https://www.hackthebox.eu/api/machines/get/{id}".format(id=identifier)
         response = self.session.get(url, verify=self.verify_cert)
         machine = MachineDetails.json_to_machinedetails(response.json())
         return machine
 
+    def owns(self):
+        url = 'https://www.hackthebox.eu/api/machines/owns'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
 
+    def difficulty(self):
+        url = 'https://www.hackthebox.eu/api/machines/difficulty'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def reviews(self):
+        url = 'https://www.hackthebox.eu/api/machines/reviews'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def todos(self):
+        url = 'https://www.hackthebox.eu/api/machines/todo'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def spawned(self):
+        url = 'https://www.hackthebox.eu/api/machines/spawned'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def terminating(self):
+        url = 'https://www.hackthebox.eu/api/machines/terminating'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def assigned(self):
+        url = 'https://www.hackthebox.eu/api/machines/assigned'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def expiry(self):
+        url = 'https://www.hackthebox.eu/api/machines/expiry'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def resetting(self):
+        url = 'https://www.hackthebox.eu/api/machines/resetting'
+        response = self.session.get(url, verify=self.verify_cert)
+        return response.json()
+
+    def start(self, machine):
+        url = 'https://www.hackthebox.eu/api/vm/vip/assign/{id}'.format(id=machine.identifier)
+        response = self.session.post(url, verify=self.verify_cert)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
+
+    def stop(self, machine):
+        url = 'https://www.hackthebox.eu/api/vm/vip/remove/{id}'.format(id=machine.identifier)
+        response = self.session.post(url, verify=self.verify_cert)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
+
+    def extend(self, machine):
+        url = 'https://www.hackthebox.eu/api/vm/vip/extend/{id}'.format(id=machine.identifier)
+        response = self.session.post(url, verify=self.verify_cert)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
+
+    def todo(self, machine):
+        url = 'https://www.hackthebox.eu/api/machines/todo/update/{id}'.format(id=machine.identifier)
+        response = self.session.post(url, verify=self.verify_cert)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
+
+    def reset(self, machine):
+        url = 'https://www.hackthebox.eu/api/vm/reset/{id}'.format(id=machine.identifier)
+        response = self.session.post(url, verify=self.verify_cert)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
+
+    def own(self, machine, flag, difficulty):
+        if not isinstance(difficulty, int) or difficulty < 1 or difficulty > 10:
+            raise TypeError('difficulty must be an integer between 1 and 10')
+        url = 'https://www.hackthebox.eu/api/machines/own'
+        payload = {'flag': flag, 'difficulty': difficulty, 'id': machine.identifier}
+        response = self.session.post(url, verify=self.verify_cert, data=payload)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
 
 if __name__ == '__main__':
 
     htb_user = os.getenv('HTB_USER')
     htb_pass = os.getenv('HTB_PASS')
 
-    client = HTBClient()
+    client = HTBClient(verify_cert=False)
     logged_in = client.login(htb_user, htb_pass)
-    print("Logged in: " + str(logged_in))
     machines = client.list_machines()
-    print("machines" + str(machines))
-    machine = client.get_machine(1)
-    print(machine)
+    for i in range(0, len(machines)):
+        print(f'{i}: {machines[i].name} (id={machines[i].identifier})')
+
+    r = client.start(machines[141])
+    print(r)
+    time.sleep(5)
+    r = client.todo(machines[141])
+    print(r)
+    time.sleep(5)
+    r = client.stop(machines[141])
+    print(r)
+
