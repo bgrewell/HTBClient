f112694f788b444a8372ec50bfa8aaaeeb7bb313
Ben Grewell <BGrewell@gmail.com>
2019-12-24 11:59:28 -0800

    refactored project layout

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HTBClient",
+    version="0.0.1",
+    author="Benjamin Grewell",
+    author_email="bgrewelldev@gmail.com",
+    description="A library to interact with the hackthebox.eu website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BGrewell/HTBClient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU :: GPLv3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
