2aa2fa8815613df3ea8c43fff47d8adac34d4a16
Ben Grewell <BGrewell@gmail.com>
2019-12-24 09:16:16 -0800

    cleaned up and added more to the README.md

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # HTBClient
+`HTBClient` is a python module that interfaces with the hackthebox.eu REST API. Since their API doesn't appear to be 
+complete there are features that are currently missing such as challenges. These will be added once they have REST
+endpoints but I have decided not to implement them now by scraping the site as I felt that would be too fragile and
+would add a significant amount of throw away code.
 
 ## Usage
+`HTBClient` can be used as a stand-alone script or imported into your own project. An example of this is my `HTB` repo
+which is a skeleton that I use to organize all the various challenges and boxes from hackthebox.eu. This repo works by
+having a basic folder layout to organize challenges and machines, then it uses the `HTBClient` to connect to the website
+and creates a folder for each machine that is found on the site but not on the file system, it also creates some metadata
+files and scripts to help with tracking and controlling the machines. To find out more about the `HTB` project click on
+this link to visit the repository.
 
-### Setting Username/Password
+#### Setting Username/Password
 If you are using `HTBClient` as a module then you need to pass the username and password when you
 create your instance. When using `HTBClient` as a module it is recommended that you get your username
 and password from an environmental variable or some place other than hard-coding it in your source
@@ -17,3 +27,12 @@ export HTB_USER=bob@somewebsite.net
 export HTB_PASS=2jli9z9e2l34jlz98890@skx9!2a&
 python3 htbclient.py <command> <arguments>
 ```
+
+## Todo/Coming Soon
+
+1. Return more meaningful responses from most of the `Machine` class methods, right now everything is the raw json from
+the website.
+
+2. Add additional meta data to the `Machine` class that will be populated by calls to expiry, terminating, active etc so
+that the additional context available in these calls is available in the machine class rather then the user having to
+make additional calls and match the results up manually.
--- htbclient.py
+++ htbclient.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python3
+
 import os
 import json
 import time
-
 import requests
 from bs4 import BeautifulSoup
 
-class MachineDetails:
 
-    def __init__(self, identifier, name, operating_system, ip, avatar, avatar_thumb, points, release, retired_date, maker, maker2, ratings_pro, ratings_sucks, user_blood, root_blood, user_owns, root_owns):
+class MachineDetails:
+    """
+    Class to represent the HTB machine details
+    """
+
+    def __init__(self, identifier, name, operating_system, ip, avatar, avatar_thumb, points, release, retired_date,
+                 maker, maker2, ratings_pro, ratings_sucks, user_blood, root_blood, user_owns, root_owns):
         self.identifier = identifier
         self.name = name
         self.operating_system = operating_system
@@ -60,7 +66,11 @@ class Machine:
     """
     Class to represent the HTB machines
     """
-    def __init__(self, identifier, name, operating_system, ip, avatar_thumb, points, release, retired_date, maker, maker2, rating, user_owns, root_owns, retired, free):
+
+    def __init__(self, session, verify_cert, identifier, name, operating_system, ip, avatar_thumb, points, release,
+                 retired_date, maker, maker2, rating, user_owns, root_owns, retired, free):
+        self.session = session
+        self.verify_cert = verify_cert
         self.identifier = identifier
         self.name = name
         self.operating_system = operating_system
@@ -83,9 +93,17 @@ class Machine:
     def __repr__(self):
         return self.name
 
+    def __request_json(self, url, data={}):
+        response = self.session.post(url, verify=self.verify_cert, data=data)
+        print(f"status_code: {response.status_code}")
+        print(f"response: {response.text}")
+        return response.json()
+
     @staticmethod
-    def json_to_machine(json_dict):
+    def json_to_machine(session, verify_cert, json_dict):
         m = Machine(
+            session,
+            verify_cert,
             json_dict['id'],
             json_dict['name'],
             json_dict['os'],
@@ -104,14 +122,44 @@ class Machine:
         )
         return m
 
-class HTBClient(object):
+    def start(self):
+        url = 'https://www.hackthebox.eu/api/vm/vip/assign/{id}'.format(id=self.identifier)
+        return self.__request_json(url)
 
+    def stop(self):
+        url = 'https://www.hackthebox.eu/api/vm/vip/remove/{id}'.format(id=self.identifier)
+        return self.__request_json(url)
+
+    def extend(self):
+        url = 'https://www.hackthebox.eu/api/vm/vip/extend/{id}'.format(id=self.identifier)
+        return self.__request_json(url)
+
+    def todo(self):
+        url = 'https://www.hackthebox.eu/api/machines/todo/update/{id}'.format(id=self.identifier)
+        return self.__request_json(url)
+
+    def reset(self):
+        url = 'https://www.hackthebox.eu/api/vm/reset/{id}'.format(id=self.identifier)
+        return self.__request_json(url)
+
+    def own(self, flag, difficulty):
+        if not isinstance(difficulty, int) or difficulty < 1 or difficulty > 10:
+            raise TypeError('difficulty must be an integer between 1 and 10')
+        url = 'https://www.hackthebox.eu/api/machines/own'
+        payload = {'flag': flag, 'difficulty': difficulty, 'id': self.identifier}
+        return self.__request_json(url, data=payload)
+
+
+class HTBClient(object):
+    additional_headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/57.0.2987.98 Safari/537.36',
+        'Referer': 'https://www.hackthebox.eu/'}
     login_url = 'https://www.hackthebox.eu/login'
-    session = None
-    additional_headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36', 'Referer': 'https://www.hackthebox.eu/'}
-    _token = None
-    logged_in = False
     verify_cert = True
+    logged_in = False
+    session = None
+    _token = None
 
     def __init__(self, verify_cert=True):
         self.verify_cert = verify_cert
@@ -131,7 +179,7 @@ class HTBClient(object):
         soup = BeautifulSoup(response.text, features='lxml')
         csrf_token = soup.select_one('meta[name="csrf-token"]')['content']
         response = self.session.get("https://www.hackthebox.eu/home/machines", verify=self.verify_cert)
-        auth_key_lindex = response.text.find("apiToken\":\"") + 11              # TODO: Clean up, sloppy way to get the apiToken
+        auth_key_lindex = response.text.find("apiToken\":\"") + 11  # TODO: Clean up, sloppy way to get the apiToken
         auth_key_rindex = response.text.find("\"", auth_key_lindex)
         auth_key = response.text[auth_key_lindex:auth_key_rindex]
         self.session.headers.update({'X-CSRF-TOKEN': csrf_token, 'X-Requested-With': 'XMLHttpRequest',
@@ -139,19 +187,19 @@ class HTBClient(object):
         self.logged_in = response.status_code == 200
         return self.logged_in
 
-    def list_machines(self):
+    def machines(self):
         if not self.logged_in:
             raise ConnectionError('You are not logged in. You must first call login()')
         list_machines_url = 'https://www.hackthebox.eu/api/machines/get/all'
         response = self.session.get(list_machines_url, verify=self.verify_cert)
         machines_json = response.json()
-        machines = []
+        machines = {}
         for machine_json in machines_json:
-            machine = Machine.json_to_machine(machine_json)
-            machines.append(machine)
+            machine = Machine.json_to_machine(self.session, self.verify_cert, machine_json)
+            machines[machine.name.lower()] = machine
         return machines
 
-    def machine(self, identifier):
+    def machine_details(self, identifier):
         if not self.logged_in:
             raise ConnectionError('You are not logged in. You must first call login()')
         url = "https://www.hackthebox.eu/api/machines/get/{id}".format(id=identifier)
@@ -174,7 +222,7 @@ class HTBClient(object):
         response = self.session.get(url, verify=self.verify_cert)
         return response.json()
 
-    def todos(self):
+    def todo(self):
         url = 'https://www.hackthebox.eu/api/machines/todo'
         response = self.session.get(url, verify=self.verify_cert)
         return response.json()
@@ -204,68 +252,39 @@ class HTBClient(object):
         response = self.session.get(url, verify=self.verify_cert)
         return response.json()
 
-    def start(self, machine):
-        url = 'https://www.hackthebox.eu/api/vm/vip/assign/{id}'.format(id=machine.identifier)
-        response = self.session.post(url, verify=self.verify_cert)
-        print(f"status_code: {response.status_code}")
-        print(f"response: {response.text}")
-        return response.json()
-
-    def stop(self, machine):
-        url = 'https://www.hackthebox.eu/api/vm/vip/remove/{id}'.format(id=machine.identifier)
-        response = self.session.post(url, verify=self.verify_cert)
-        print(f"status_code: {response.status_code}")
-        print(f"response: {response.text}")
-        return response.json()
-
-    def extend(self, machine):
-        url = 'https://www.hackthebox.eu/api/vm/vip/extend/{id}'.format(id=machine.identifier)
-        response = self.session.post(url, verify=self.verify_cert)
-        print(f"status_code: {response.status_code}")
-        print(f"response: {response.text}")
-        return response.json()
-
-    def todo(self, machine):
-        url = 'https://www.hackthebox.eu/api/machines/todo/update/{id}'.format(id=machine.identifier)
-        response = self.session.post(url, verify=self.verify_cert)
-        print(f"status_code: {response.status_code}")
-        print(f"response: {response.text}")
-        return response.json()
-
-    def reset(self, machine):
-        url = 'https://www.hackthebox.eu/api/vm/reset/{id}'.format(id=machine.identifier)
-        response = self.session.post(url, verify=self.verify_cert)
-        print(f"status_code: {response.status_code}")
-        print(f"response: {response.text}")
-        return response.json()
-
-    def own(self, machine, flag, difficulty):
-        if not isinstance(difficulty, int) or difficulty < 1 or difficulty > 10:
-            raise TypeError('difficulty must be an integer between 1 and 10')
-        url = 'https://www.hackthebox.eu/api/machines/own'
-        payload = {'flag': flag, 'difficulty': difficulty, 'id': machine.identifier}
-        response = self.session.post(url, verify=self.verify_cert, data=payload)
-        print(f"status_code: {response.status_code}")
-        print(f"response: {response.text}")
-        return response.json()
 
 if __name__ == '__main__':
 
     htb_user = os.getenv('HTB_USER')
     htb_pass = os.getenv('HTB_PASS')
 
+    if htb_user is None or htb_pass is None:
+        print('[!] Error: Username and Password must be set in environmental variables.')
+        print('[!] Below is an example')
+        print('\nexport HTB_USER=someone@address.com')
+        print('export HTB_PASS=hopefullyagoodpassword')
+        exit(-1)
+
     client = HTBClient()
-    logged_in = client.login(htb_user, htb_pass)
-    machines = client.list_machines()
-    for i in range(0, len(machines)):
-        print(f'{i}: {machines[i].name} (id={machines[i].identifier})')
+    client.login(htb_user, htb_pass)
+    machines = client.machines()
+    idx = 1
+    for key, value in machines.items():
+        print(f'{idx}: {machines[key].name} (id={machines[key].identifier})')
+        idx += 1
 
-    r = client.start(machines[141])
+    player_two = machines['playertwo']
+    r = player_two.start()
+    print(r)
+    print(client.assigned())
+    time.sleep(5)
+    r = player_two.todo()
     print(r)
     time.sleep(5)
-    r = client.todo(machines[141])
+    r = player_two.stop()
     print(r)
-    time.sleep(5)
-    r = client.stop(machines[141])
-    print(r)
+    print(client.terminating())
 
+    print(client.todo())
+
+    print(client.expiry())
