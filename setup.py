5104918c49f02cf0d0834d0b87c24005aa7c81e6
Ben Grewell <BGrewell@gmail.com>
2020-01-02 15:12:02 -0800

    reved version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="HTBClient",
-    version="0.0.3",
+    version="0.1.0",
     author="Benjamin Grewell",
     author_email="bgrewelldev@gmail.com",
     description="A library to interact with the hackthebox.eu website",
