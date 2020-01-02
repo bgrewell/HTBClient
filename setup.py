49426b5d6e57651d42fd65fd4af7a61b9af790ed
Ben Grewell <BGrewell@gmail.com>
2020-01-02 12:43:25 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HTBClient",
+    version="0.0.3",
+    author="Benjamin Grewell",
+    author_email="bgrewelldev@gmail.com",
+    description="A library to interact with the hackthebox.eu website",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BGrewell/HTBClient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
